760cb2b28ec04c4d358cbd21a61a903430ca3f88
nuriamafe <nuriamafe256@gmail.com>
2024-01-17 16:10:02 +0100

    Preview group

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,18 @@
 import React, { useState } from "react";
 import { Button, Image } from "antd";
-import foto from "../assets/cul.jpg";
+import cul from "../assets/cul.jpg";
+import foto0 from "../assets/foto0.jpg";
+import foto1 from "../assets/foto1.jpg";
+import foto2 from "../assets/foto2.jpg";
+import foto3 from "../assets/foto3.jpg";
 
 function HomePage() {
   const [visible, setVisible] = useState(false);
   return (
     <>
-      <Button onClick={() => setVisible(true)}>Click me!</Button>
-      <Image
-        style={{
-          display: "none",
-        }}
-        src={foto}
-        preview={{
-          visible,
-          src: foto,
-          onVisibleChange: (value) => {
-            setVisible(value);
-          },
-        }}
-      />
+      <Image.PreviewGroup items={[cul, foto0, foto1, foto2, foto3]}>
+        <Image width={200} src={cul} />
+      </Image.PreviewGroup>
     </>
   );
 }
